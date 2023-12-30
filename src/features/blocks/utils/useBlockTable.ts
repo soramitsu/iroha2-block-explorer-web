a0e4f00f0774001ad4916e3c09f474245510ba35
horizenight <kshitijroodkee1@gmail.com>
2023-12-30 08:18:10 +0530

    [feature] #27: Fix useBlockTable
    Signed-off-by: horizenight <kshitijroodkee1@gmail.com>

--- src/features/blocks/utils/useBlockTable.ts
+++ src/features/blocks/utils/useBlockTable.ts
@@ -1,4 +1,4 @@
-import { Ref, ref, computed } from 'vue';
+import { Ref, ref } from 'vue';
 import { transactionModel } from '~entities/transaction';
 
 export type TableFetchFn<T> = (params: PaginationParams) => Promise<Paginated<T>>;
@@ -13,46 +13,48 @@ export type TablePagination = {
 export function useBlockTable() {
   const loading = ref(false);
   const selectedTransaction: Ref<TransactionDto[]> = ref([]);
+  const items: Ref<Transaction[]> = ref([]);
   const pagination: Ref<TablePagination> = ref({
     page: 1,
     page_size: 10,
     pages: 1,
     total: 10,
   });
-
-  const items = computed(() => {
+  function fetch() {
     const res = transactionModel.fetchBlocksList(selectedTransaction.value, {
       page: pagination.value.page,
       page_size: pagination.value.page_size,
     });
-
+    items.value = res.data;
     pagination.value = {
       ...res.pagination,
       pages: Math.ceil(res.pagination.total / res.pagination.page_size),
     };
-
-    return res.data;
-  });
+  }
 
   function setTransaction(transaction: TransactionDto[]) {
     selectedTransaction.value = transaction;
+    fetch();
   }
 
   function nextPage() {
     if (pagination.value.page < pagination.value.pages) {
       pagination.value.page += 1;
+      fetch();
     }
   }
 
   function prevPage() {
     if (pagination.value.page > 1) {
       pagination.value.page -= 1;
+      fetch();
     }
   }
 
   function setPage(index: number) {
     if (index !== pagination.value.page) {
       pagination.value.page = index;
+      fetch();
     }
   }
 
@@ -60,6 +62,7 @@ export function useBlockTable() {
     if (n !== pagination.value.page_size) {
       pagination.value.page_size = n;
       pagination.value.page = 1;
+      fetch();
     }
   }
 
