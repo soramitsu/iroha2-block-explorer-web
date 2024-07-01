1b21f65ddeb2046fa368858afb7104a1c1a27b67
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-02 00:36:16 +0300

    feat(types): add necessary types

---
+++ src/core/types/common.ts
@@ -0,0 +1,20 @@
+export interface Tagged<T, Content> {
+  t: T
+  c: Content
+}
+
+export interface PaginationParamsDto {
+  page: number
+  page_size: number
+}
+
+export interface PaginationInfo {
+  page: number
+  page_size: number
+  total: number
+}
+
+export interface Paginated<T> {
+  pagination: PaginationInfo
+  data: T[]
+}
---
+++ src/core/types/transactions.ts
@@ -0,0 +1,2 @@
+export type TRANSACTION_STATUS = 'committed' | 'rejected' | null;
+export type TRANSACTIONS_TABS = 'all' | 'transfers' | 'mints' | 'burns' | 'grants' | 'revokes';
