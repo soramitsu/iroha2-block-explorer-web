2eab206932a528d163fa06032d8ddf18a81e9240
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-02 22:08:37 +0300

    fix(instance): fix sonarqube issue

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL as string) ?? '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL ?? '/api/v1',
   timeout: 30000,
   responseType: 'json',
 });
