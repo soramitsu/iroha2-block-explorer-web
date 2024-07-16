7c061dbdae104535b28e208e01f73ec4a91ae92d
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-16 20:31:59 +0300

    refactor(money-formatters): use Intl formatter instead

--- src/shared/utils/money-formatters.ts
+++ src/shared/utils/money-formatters.ts
@@ -1,14 +1,11 @@
 import { applicationCurrency } from '@/shared/config';
 
 export function numberFormatter(value: number | string) {
-  const dividedValue = value.toString().split('.');
+  const formatter = new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: 10,
+  });
 
-  const commaRegExp = new RegExp(/\B(?=(\d{3})+(?!\d))/g);
-
-  const mainValue = dividedValue[0].replace(commaRegExp, ',');
-  const restValue = dividedValue[1] ? '.' + dividedValue[1] : '';
-
-  return mainValue + restValue;
+  return formatter.format(Number(value));
 }
 
 export function formatMoney(value: number | string) {
