347f009af81bee3005638bf982cdb819b14c0fc6
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-16 15:45:20 +0300

    feat(useErrorHandlers): add error handling

---
+++ src/shared/ui/composables/useErrorHandlers.ts
@@ -0,0 +1,26 @@
+import { useNotifications } from './notifications';
+
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (typeof error === 'string') {
+    return error;
+  }
+
+  return '';
+}
+
+export const useErrorHandlers = () => {
+  const notifications = useNotifications();
+
+  function handleUnknownError(error: unknown) {
+    console.error(error);
+
+    const errorMessage = getErrorMessage(error);
+    notifications.error(errorMessage);
+  }
+
+  return { handleUnknownError } as const;
+};
