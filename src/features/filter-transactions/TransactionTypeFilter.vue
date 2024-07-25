3cfc86df7ebd335907be218b85352c43d3e4cabf
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-25 11:33:27 +0300

    refactor(TransactionTypeFilter): document tabs

--- src/features/filter-transactions/TransactionTypeFilter.vue
+++ src/features/filter-transactions/TransactionTypeFilter.vue
@@ -11,6 +11,8 @@ import type * as ftm from './model';
 import { useVModel } from '@vueuse/core';
 import BaseTabs from '@/shared/ui/components/BaseTabs.vue';
 import type { AdaptiveOptions } from '@/shared/types';
+import { computed } from 'vue';
+import { defaultTransactionTypeOptions, transactionTypeOptions } from './model';
 
 interface Props {
   modelValue: ftm.TransactionTypeTabs | ftm.DefaultTransactionTypeTabs
@@ -23,14 +25,7 @@ type Emits = (e: 'update:modelValue', value: ftm.TransactionTypeTabs) => void;
 const props = defineProps<Props>();
 const emit = defineEmits<Emits>();
 
-const tabs = [
-  { label: props.defaultOptions ? 'transactions' : 'all', value: props.defaultOptions ? 'transactions' : 'all' },
-  { label: 'transfers', value: 'transfers' },
-  { label: 'mints', value: 'mints' },
-  { label: 'burns', value: 'burns' },
-  { label: 'grants', value: 'grants' },
-  { label: 'revokes', value: 'revokes' },
-];
+const tabs = computed(() => (props.defaultOptions ? defaultTransactionTypeOptions : transactionTypeOptions));
 
 const model = useVModel(props, 'modelValue', emit);
 </script>
--- src/features/filter-transactions/model.ts
+++ src/features/filter-transactions/model.ts
@@ -1,3 +1,21 @@
 export type TransactionStatus = 'committed' | 'rejected' | null;
 export type TransactionTypeTabs = 'all' | 'transfers' | 'mints' | 'burns' | 'grants' | 'revokes';
 export type DefaultTransactionTypeTabs = 'transactions' | 'transfers' | 'mints' | 'burns' | 'grants' | 'revokes';
+
+export const defaultTransactionTypeOptions = [
+  { label: 'transactions', value: 'transactions' },
+  { label: 'transfers', value: 'transfers' },
+  { label: 'mints', value: 'mints' },
+  { label: 'burns', value: 'burns' },
+  { label: 'grants', value: 'grants' },
+  { label: 'revokes', value: 'revokes' },
+];
+
+export const transactionTypeOptions = [
+  { label: 'all', value: 'all' },
+  { label: 'transfers', value: 'transfers' },
+  { label: 'mints', value: 'mints' },
+  { label: 'burns', value: 'burns' },
+  { label: 'grants', value: 'grants' },
+  { label: 'revokes', value: 'revokes' },
+];
