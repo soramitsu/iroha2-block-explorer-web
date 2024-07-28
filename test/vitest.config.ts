c71cf6920dc198c5f497dff79cd45ff47b4b546c
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-29 00:24:19 +0300

    feat(package.json): add vitest package

---
+++ test/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vite';
+import viteConfig from '../vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+    },
+  })
+);
