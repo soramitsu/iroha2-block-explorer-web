fbd738bd5129de97d67edda5122f70a86b86ec80
Vladislav Popov <papou@soramitsu.co.jp>
2024-07-29 23:50:12 +0300

    feat(package.json): update vite version and remove vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import path from 'path';
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import svg from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+  },
   build: {
     target: 'esnext',
   },
