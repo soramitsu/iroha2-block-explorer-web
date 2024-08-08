08867927553f63496eb6e619f5f2e3bb9474ff42
Vladislav Popov <papou@soramitsu.co.jp>
2024-08-08 19:32:01 +0300

    feat(filter-transactions): add assets sort

--- src/features/filter-transactions/model.ts
+++ src/features/filter-transactions/model.ts
@@ -1,11 +1,17 @@
 export type Status = 'committed' | 'rejected' | null;
 export type TabDefaultScreen = 'all' | 'transfers' | 'mints' | 'burns' | 'grants' | 'revokes';
 export type TabBlocksScreen = 'transactions' | 'transfers' | 'mints' | 'burns' | 'grants' | 'revokes';
+export type TabAssetsSort = 'recent' | 'value';
 
 export interface TabItem<T = string> {
   i18nKey: string
   value: T
 }
+
+export const sortOptions: TabItem<TabAssetsSort>[] = [
+  { i18nKey: 'mostRecent', value: 'recent' },
+  { i18nKey: 'mostValue', value: 'value' },
+];
 
 export const blockOptions: TabItem<TabBlocksScreen>[] = [
   { i18nKey: 'transactions', value: 'transactions' },
