e290a580070b944b8f6f25d4647265708e91ff87
Vladislav Popov <papou@soramitsu.co.jp>
2024-08-23 14:10:24 +0300

    feat(BaseTable): add prop

--- src/shared/ui/components/BaseTable.vue
+++ src/shared/ui/components/BaseTable.vue
@@ -86,15 +86,16 @@ import { computed } from 'vue';
 import { useWindowSize } from '@vueuse/core';
 import ArrowIcon from '@/shared/ui/icons/arrow.svg';
 import BaseLoading from './BaseLoading.vue';
-import type { TablePagination } from '@/shared/lib/table';
 import BaseDropdown from '@/shared/ui/components/BaseDropdown.vue';
+import type { Pagination } from '@/shared/api/dto';
 
 interface Props {
   loading: boolean
-  pagination?: TablePagination | null
+  pagination?: Pagination | null
   items: T[]
   containerClass: string
   breakpoint?: string | number
+  sticky?: boolean
 }
 
 interface Emits {
@@ -125,9 +126,23 @@ const segmentInfo = computed(() => {
   if (!props.pagination) return '';
 
   const p = props.pagination;
-  const start = (p.page - 1) * p.page_size + 1;
-  const end = p.page * p.page_size;
-  return `${start}—${end > p.total ? p.total : end} of ${p.total}`;
+
+  if (!p.total_items) p.total_items = props.items.length;
+
+  if (props.sticky) {
+    if (p.per_page > props.items.length) return `${props.items.length}—1 of ${p.total_items}`;
+
+    const start = (p.page - 1) * p.per_page + props.items.length;
+
+    const end = start - props.items.length + 1;
+
+    return `${start}—${end} of ${p.total_items}`;
+  }
+
+  const start = (p.page - 1) * p.per_page + 1;
+  const end = p.page * p.per_page;
+
+  return `${start}—${end > p.total_items ? p.total_items : end} of ${p.total_items}`;
 });
 
 const numbers = computed(() => {
@@ -139,33 +154,62 @@ const numbers = computed(() => {
   const offset = isMobile ? 1 : 3;
 
   const p = props.pagination;
-  if (p.pages < max) {
-    return new Array(p.pages).fill(0).map((_, i) => i + 1);
+
+  if (!p.total_pages) p.total_pages = 1;
+
+  if (p.total_pages < max) {
+    const numbers = new Array(p.total_pages).fill(0).map((_, i) => i + 1);
+
+    return props.sticky ? numbers.reverse() : numbers;
   }
 
   let start = p.page - offset;
   let end = p.page + offset;
 
   if (p.page < side) {
+    if (props.sticky)
+      return Array(side)
+        .fill(p.total_pages)
+        .map<string | number>((_, i) => _ - i)
+        .concat(['. . .', 1]);
+
     return Array(side)
       .fill(0)
       .map<string | number>((_, i) => i + 1)
-      .concat(['. . .', p.pages]);
+      .concat(['. . .', p.total_pages]);
   }
 
-  if (p.page > p.pages - side + 1) {
+  if (p.page > p.total_pages - side + 1) {
+    if (props.sticky)
+      return Array(side)
+        .fill(p.total_pages)
+        .map<string | number>((_, i) => _ - i)
+        .concat(['. . .', 1]);
+
     return [1, '. . .'].concat(
       Array(side)
         .fill(0)
-        .map((_, i) => p.pages - i)
+        .map((_, i) => p.per_page - i)
         .reverse()
     );
   }
 
   start = start < 1 ? 1 : start;
-  end = end > p.pages ? p.pages : end;
+  end = end > p.total_pages ? p.total_pages : end;
 
-  return [1, '. . .', ...new Array(end - start + 1).fill(0).map((_, i) => i + start), '. . .', p.pages];
+  if (props.sticky)
+    return [
+      p.total_pages,
+      '. . .',
+      ...new Array(end - start + 1)
+        .fill(0)
+        .map((_, i) => i + start)
+        .reverse(),
+      '. . .',
+      1,
+    ];
+
+  return [1, '. . .', ...new Array(end - start + 1).fill(0).map((_, i) => i + start), '. . .', p.total_pages];
 });
 
 const sizeOptions = [
@@ -188,7 +232,7 @@ const sizeOptions = [
 ];
 
 const pageSizeModel = computed({
-  get: () => props.pagination?.page_size ?? 0,
+  get: () => props.pagination?.per_page ?? 0,
   set: (v) => emit('setSize', v),
 });
 </script>
