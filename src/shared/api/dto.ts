2d8bc43cf79010163a9fd01df4af69e4cae4cbee
Vladislav Popov <papou@soramitsu.co.jp>
2024-09-03 08:46:24 +0300

    feat(dto): rewrite dto using zod schemas

--- src/shared/api/dto.ts
+++ src/shared/api/dto.ts
@@ -1,183 +1,228 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable camelcase */
-import type { JsonObject } from 'type-fest';
+import { z } from 'zod';
+import BigNumber from 'bignumber.js';
+import invariant from 'tiny-invariant';
 
-export {};
+const paginationSchema = z.object({
+  page: z.number(),
+  per_page: z.number(),
+  // TODO: remove nullable when backend is ready
+  total_pages: z.number().nullable(),
+  total_items: z.number().nullable(),
+});
 
-declare global {
-  interface Tagged<T, Content> {
-    t: T
-    c: Content
+export type Pagination = z.infer<typeof paginationSchema>;
+
+export const paginatedSchema = <T extends z.ZodType>(item: T) =>
+  z.object({
+    pagination: paginationSchema,
+    items: item.array(),
+  });
+
+export interface Paginated<T> {
+  pagination: Pagination
+  items: T[]
+}
+
+const paginationParamsSchema = z
+  .object({
+    page: z.number(),
+    per_page: z.number(),
+  })
+  .partial();
+
+export type PaginationParams = z.infer<typeof paginationParamsSchema>;
+
+const metadataSchema = z.record(z.string(), z.any());
+
+const accountIdSchema = z.string().brand('AccountId');
+const domainIdSchema = z.string().brand('DomainId');
+
+class AssetDefinitionId {
+  private assetName = '';
+  private assetDomain = '';
+
+  public constructor(name: string, domain: string) {
+    this.assetName = name;
+    this.assetDomain = domain;
   }
 
-  interface PaginationParams {
-    page: number
-    page_size: number
+  public get name() {
+    return this.assetName;
   }
-
-  interface Paginated<T> {
-    pagination: {
-      page: number
-      page_size: number
-      total: number
-    }
-    data: T[]
+  public get domain() {
+    return this.assetDomain;
   }
-
-  interface Asset {
-    account_id: string
-    definition_id: string
-    value: AssetValue
-  }
-
-  type AssetValue =
-    | Tagged<'Quantity', string>
-    | Tagged<'BigQuantity', string>
-    | Tagged<'Fixed', string>
-    | Tagged<'Store', JsonObject>;
-
-  type AssetValueType = 'Quantity' | 'BigQuantity' | 'Fixed' | 'Store';
-
-  interface AssetDefinition {
-    id: string
-    value_type: AssetValueType
-    mintable: Mintable
-  }
-
-  type Mintable = 'Once' | 'Infinitely' | 'Not';
-
-  interface Peer {
-    address: string
-    public_key: PublicKey
-  }
-
-  interface Role {
-    id: string
-    permissions: PermissionToken[]
-  }
-
-  interface PermissionToken {
-    name: string
-    params: any
-  }
-
-  interface Account {
-    id: string
-    assets: Asset[]
-    signatories?: PublicKey[]
-    permission_tokens?: PermissionToken[]
-    roles: Role[]
-    signature_check_condition?: any
-    metadata: any
-  }
-
-  interface Domain {
-    id: string
-    accounts: Account[]
-    asset_definitions: AssetDefinition[]
-    logo: null | string
-    metadata: any
-    /**
-     * amount of triggers, always 0 for now
-     */
-    triggers: number
-  }
-
-  interface PublicKey {
-    digest_function: string
-    payload: string
-  }
-
-  interface Status {
-    peers: string
-    blocks: string
-    txs: string
-    uptime: {
-      secs: string
-      nanos: string
-    }
-  }
-
-  export interface BlockShallow {
-    height: number
-    /**
-     * ISO DateTime
-     */
-    timestamp: string
-    block_hash: string
-    /**
-     * Transactions count
-     */
-    transactions: number
-    /**
-     * Rejected transactions count
-     */
-    rejected_transactions: number
-  }
-
-  export interface Block {
-    height: number
-    /**
-     * See {@link BlockShallow.timestamp}
-     */
-    timestamp: string
-    block_hash: string
-    parent_block_hash: string
-    rejected_transactions_merkle_root_hash: string
-    invalidated_blocks_hashes: string[]
-    /**
-     * List of serialized {@link @iroha2/data-model#VersionedValidTransaction}
-     */
-    transactions: string[]
-    /**
-     * List of serialized {@link @iroha2/data-model#VersionedRejectedTransaction}
-     */
-    rejected_transactions: string[]
-    /**
-     * List of hashes. WIP always empty
-     */
-    view_change_proofs: string[]
-  }
-
-  export interface TransactionDto {
-    /**
-     * WIP zeroed
-     */
-    hash: string
-    block_hash: string
-    // TODO: bloch_height is missing from backend response
-    block_height: number
-    payload: TransactionPayload
-    signatures: Signature[]
-    rejection_reason?: string
-  }
-
-  export interface TransactionPayload {
-    account_id: string
-    instructions: TransactionInstructions
-    /**
-     * ISO timestamp
-     */
-    creation_time: string
-    time_to_live_ms: number | null
-    nonce: number | null
-    metadata: any
-  }
-
-  /**
-   * `Instructions` `string[]` - list of serialized
-   * {@link @iroha2/data-model#Instruction}
-   */
-  export type TransactionInstructions = Tagged<'Instructions', string[]> | Tagged<'Wasm', undefined>;
-
-  export interface Signature {
-    /**
-     * Public key's multihash
-     */
-    public_key: string
-    /**
-     * Hex binary
-     */
-    payload: string
+  public toString() {
+    return this.assetName + '#' + this.assetDomain;
   }
 }
+
+export function transformToAssetDefinitionId(assetDefinitionId: string): AssetDefinitionId {
+  const parts = assetDefinitionId.split('#');
+  invariant(parts.length === 2, 'Invalid assetDefinitionId format');
+
+  const [asset, asset_domain] = parts;
+
+  return new AssetDefinitionId(asset, asset_domain);
+}
+
+class AssetId {
+  private assetName = '';
+  private assetDomain = '';
+  private accountSignatory = '';
+  private accountDomain = '';
+
+  public constructor(data: { asset: string, asset_domain: string, signatory: string, account_domain: string }) {
+    this.assetName = data.asset;
+    this.assetDomain = data.asset_domain;
+    this.accountSignatory = data.signatory;
+    this.accountDomain = data.account_domain;
+  }
+
+  public get asset() {
+    return this.assetName;
+  }
+  public get domain() {
+    return this.assetDomain ? this.assetDomain : this.accountDomain;
+  }
+  public get signatory() {
+    return this.accountSignatory;
+  }
+  public get account_domain() {
+    return this.accountDomain;
+  }
+  public get asset_definition_id() {
+    return this.assetName + '#' + this.domain;
+  }
+  public toString() {
+    return this.assetName + '#' + this.assetDomain + '#' + this.accountSignatory + '@' + this.accountDomain;
+  }
+}
+
+export function transformToAssetId(assetId: string): AssetId {
+  const assetIdParts = assetId.split('#');
+  invariant(assetIdParts.length === 3, 'Invalid assetId format');
+  const [asset, asset_domain, account] = assetIdParts;
+
+  const accountParts = account.split('@');
+  invariant(accountParts.length === 2, 'Invalid account format');
+  const [signatory, account_domain] = accountParts;
+
+  const data = {
+    asset,
+    asset_domain,
+    signatory,
+    account_domain,
+  };
+
+  return new AssetId(data);
+}
+
+export const accountSchema = z.object({
+  id: accountIdSchema,
+  metadata: metadataSchema,
+  owned_assets: z.number(),
+  owned_domains: z.number(),
+});
+
+export type Account = z.infer<typeof accountSchema>;
+
+export interface AssetSearchParams extends PaginationParams {
+  owned_by?: string
+}
+
+export const assetSchema = z.object({
+  id: z.string().transform(transformToAssetId),
+  value: z.discriminatedUnion('kind', [
+    z.object({ kind: z.literal('Numeric'), value: z.string().transform((value) => BigNumber(value)) }),
+    z.object({ kind: z.literal('Store'), metadata: metadataSchema }),
+  ]),
+});
+
+export type Asset = z.infer<typeof assetSchema>;
+
+export const assetDefinitionSchema = z.object({
+  id: z.string().transform(transformToAssetDefinitionId),
+  type: z.enum(['Numeric', 'Store']),
+  logo: z.string().nullable(),
+  assets: z.number(),
+  metadata: metadataSchema,
+  mintable: z.enum(['Infinitely', 'Once', 'Not']),
+  owned_by: accountIdSchema,
+});
+
+export type AssetDefinition = z.infer<typeof assetDefinitionSchema>;
+
+export const domainSchema = z.object({
+  id: domainIdSchema,
+  logo: z.string().nullable(),
+  metadata: metadataSchema,
+  owned_by: z.string(),
+  accounts: z.number(),
+  assets: z.number(),
+});
+
+export type Domain = z.infer<typeof domainSchema>;
+
+export interface TransactionSearchParams extends PaginationParams {
+  authority?: string
+  block_hash?: string
+}
+
+export const transactionSchema = z.object({
+  authority: z.string(),
+  hash: z.string(),
+  block_hash: z.string(),
+  error: z.boolean(),
+  created_at: z.string().transform((x) => new Date(x)),
+  instructions: z.enum(['Instructions', 'Wasm']),
+});
+
+export const detailedTransactionSchema = z.object({
+  authority: z.string(),
+  hash: z.string(),
+  block_hash: z.string(),
+  created_at: z.string().transform((x) => new Date(x)),
+  instructions: z.enum(['Instructions', 'Wasm']),
+  error: z.record(z.string(), z.any()).nullable(),
+  metadata: metadataSchema,
+  nonce: z.number().nullable(),
+  signature: z.string(),
+  time_to_live: z
+    .object({
+      ms: z.number().min(0),
+    })
+    .nullable(),
+});
+
+export type Transaction = z.infer<typeof transactionSchema>;
+export type DetailedTransaction = z.infer<typeof detailedTransactionSchema>;
+
+export const blockSchema = z.object({
+  hash: z.string(),
+  height: z.number(),
+  created_at: z.string().transform((x) => new Date(x)),
+  prev_block_hash: z.string().nullable(),
+  transactions_hash: z.string(),
+  transactions_rejected: z.number(),
+  transactions_total: z.number(),
+  consensus_estimation: z.object({
+    ms: z.number().min(0),
+  }),
+});
+
+export type Block = z.infer<typeof blockSchema>;
+
+export const peerStatusSchema = z.object({
+  blocks: z.number(),
+  peers: z.number(),
+  queue_size: z.number(),
+  txs_accepted: z.number(),
+  txs_rejected: z.number(),
+  uptime: z.object({
+    ms: z.number().min(0),
+  }),
+  view_changes: z.number(),
+});
+
+export type PeerStatus = z.infer<typeof peerStatusSchema>;
