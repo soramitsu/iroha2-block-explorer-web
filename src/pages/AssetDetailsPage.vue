3f8058fe483dbfa33d048450764c3eb3d17c35e6
Vladislav Popov <papou@soramitsu.co.jp>
2024-09-12 11:20:33 +0300

    fix(AssetDetailsPage): remove transactions table

--- src/pages/AssetDetailsPage.vue
+++ src/pages/AssetDetailsPage.vue
@@ -3,27 +3,15 @@ import { useRouter } from 'vue-router';
 import { computed, onMounted, ref } from 'vue';
 import * as http from '@/shared/api';
 import BaseContentBlock from '@/shared/ui/components/BaseContentBlock.vue';
-import { useTable } from '@/shared/lib/table';
-import BaseTable from '@/shared/ui/components/BaseTable.vue';
-import { type filterTransactionsModel as ftm, TransactionStatusFilter } from '@/features/filter-transactions';
-import BaseHash from '@/shared/ui/components/BaseHash.vue';
-import { useWindowSize } from '@vueuse/core';
 import { useErrorHandlers } from '@/shared/ui/composables/useErrorHandlers';
 import BaseLoading from '@/shared/ui/components/BaseLoading.vue';
 import DataField from '@/shared/ui/components/DataField.vue';
-import { elapsed } from '@/shared/lib/time';
 import type { AssetDefinition } from '@/shared/api/schemas';
 import { AssetDefinitionIdSchema } from '@/shared/api/schemas';
-import TransactionStatus from '@/entities/transaction/TransactionStatus.vue';
 
 const router = useRouter();
 
 const { handleUnknownError } = useErrorHandlers();
-
-const HASH_BREAKPOINT = 800;
-const { width } = useWindowSize();
-
-const hashType = computed(() => (width.value < HASH_BREAKPOINT ? 'medium' : 'full'));
 
 const assetDefinitionId = computed(() => {
   const id = router.currentRoute.value.params['id'];
@@ -38,19 +26,12 @@ onMounted(async () => {
   try {
     isFetchingAsset.value = true;
     asset.value = await http.fetchAssetDefinition(assetDefinitionId.value);
-
-    await transactionsTable.fetch();
   } catch (e) {
     handleUnknownError(e);
   } finally {
     isFetchingAsset.value = false;
   }
 });
-
-const transactionStatus = ref<ftm.Status>(null);
-
-// FIXME: this loads ALL transactions, not only related to the asset
-const transactionsTable = useTable(http.fetchTransactions);
 </script>
 
 <template>
@@ -95,42 +76,6 @@ const transactionsTable = useTable(http.fetchTransactions);
         </div>
       </template>
     </BaseContentBlock>
-    <BaseContentBlock
-      :title="$t('assets.assetTransactions')"
-      class="asset-details__transactions"
-    >
-      <div class="asset-details__transactions-filters content-row">
-        <TransactionStatusFilter
-          v-model="transactionStatus"
-          class="asset-details__transactions-filters-status"
-        />
-      </div>
-
-      <BaseTable
-        :loading="transactionsTable.loading.value"
-        :items="transactionsTable.items.value"
-        container-class="asset-details__transactions-container"
-      >
-        <template #row="{ item }">
-          <div class="asset-details__transactions-row">
-            <TransactionStatus
-              type="tooltip"
-              class="asset-details__transactions-row-icon"
-              :committed="item.status === 'Committed'"
-            />
-
-            <div class="asset-details__transactions-row-data">
-              <BaseHash
-                :type="hashType"
-                :hash="item.hash"
-                :link="`/transactions/${item.hash}`"
-              />
-              <span class="asset-details__transactions-row-data-time row-text">{{ $t('time.min', [elapsed.allMinutes(item.created_at)]) }} {{ $t('time.ago') }}</span>
-            </div>
-          </div>
-        </template>
-      </BaseTable>
-    </BaseContentBlock>
   </div>
 </template>
 
@@ -174,45 +119,6 @@ const transactionsTable = useTable(http.fetchTransactions);
 
       .base-link {
         @include tpg-s3;
-      }
-    }
-  }
-
-  &__transactions {
-    &-filters {
-      border-top: 0;
-      display: flex;
-      padding: size(2) size(4);
-    }
-
-    &-container {
-      display: grid;
-      .content-row:last-child {
-        border-bottom: 1px solid theme-color('border-primary');
-      }
-    }
-
-    &-row {
-      @include xxs {
-        padding: 0 size(2);
-      }
-
-      width: 100%;
-      display: flex;
-
-      &-data {
-        &-time {
-          @include tpg-s5;
-        }
-      }
-
-      &-icon {
-        display: none;
-        margin-right: size(2);
-
-        @include xs {
-          display: grid;
-        }
       }
     }
   }
