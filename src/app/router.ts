3bfad8deec6c5f02588a67109e46aaca31fd2419
Vladislav Popov <papou@soramitsu.co.jp>
2024-10-03 09:55:55 +0300

    refactor(pages): rename pages according vue style guide

--- src/app/router.ts
+++ src/app/router.ts
@@ -9,57 +9,57 @@ const routes = [
   {
     path: '/blocks',
     name: 'blocks-list',
-    component: () => import('@/pages/BlocksListPage.vue'),
+    component: () => import('@/pages/BlocksList.vue'),
   },
   {
     path: '/blocks/:heightOrHash',
     name: 'blocks-details',
-    component: () => import('@/pages/BlockDetailsPage.vue'),
+    component: () => import('@/pages/BlockDetails.vue'),
   },
 
   {
     path: '/assets',
     name: 'assets-list',
-    component: () => import('@/pages/AssetsListPage.vue'),
+    component: () => import('@/pages/AssetsList.vue'),
   },
   {
     path: '/assets/:id',
     name: 'asset-details',
-    component: () => import('@/pages/AssetDetailsPage.vue'),
+    component: () => import('@/pages/AssetDetails.vue'),
   },
   {
     path: '/accounts',
     name: 'accounts-list',
-    component: () => import('@/pages/AccountsListPage.vue'),
+    component: () => import('@/pages/AccountsList.vue'),
   },
   {
     path: '/accounts/:id',
     name: 'account-details',
-    component: () => import('@/pages/AccountDetailsPage.vue'),
+    component: () => import('@/pages/AccountDetails.vue'),
   },
   {
     path: '/domains',
     name: 'domains-list',
-    component: () => import('@/pages/DomainsListPage.vue'),
+    component: () => import('@/pages/DomainsList.vue'),
   },
   {
     path: '/domains/:id',
     name: 'domain-details',
-    component: () => import('@/pages/DomainDetailsPage.vue'),
+    component: () => import('@/pages/DomainDetails.vue'),
   },
   {
     path: '/transactions',
     name: 'transactions-list',
-    component: () => import('@/pages/TransactionsListPage.vue'),
+    component: () => import('@/pages/PageTransactionsList.vue'),
   },
   {
     path: '/transactions/:hash',
     name: 'transaction-details',
-    component: () => import('@/pages/TransactionDetailsPage.vue'),
+    component: () => import('@/pages/TransactionDetails.vue'),
   },
   {
     path: '/:pathMatch(.*)*',
-    component: () => import('@/pages/NotFoundPage.vue'),
+    component: () => import('@/pages/NotFound.vue'),
   },
 ];
 
