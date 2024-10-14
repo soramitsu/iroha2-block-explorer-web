aa0b3f145e9630b6cce5c8593f13bcfe383df326
Vladislav Popov <papou@soramitsu.co.jp>
2024-10-14 13:53:18 +0300

    feat(time.test): add tests

--- src/shared/lib/time.test.ts
+++ src/shared/lib/time.test.ts
@@ -1,9 +1,39 @@
 import { expect, test } from 'vitest';
-import { formatUTC } from '@/shared/lib/time';
+import { formatUTC, getTimeAgo } from '@/shared/lib/time';
+import { mount } from '@vue/test-utils';
+import { i18n } from '@/shared/lib/localization';
 
 test.each([
   [new Date('2024-09-11T07:22:47.157Z'), 'Sep-11-2024 10:22:47 AM UTC'],
   [new Date(1726641137724), 'Sep-18-2024 09:32:17 AM UTC'],
 ])('format %f to %s', (input, expected) => {
   expect(formatUTC(input)).toBe(expected);
+});
+
+test.each([
+  [new Date(Date.now() - 11000), '0 min 11 sec ago'],
+  [new Date(Date.now() - 3599000), '59 min 59 sec ago'],
+  [new Date(Date.now() - 4000000), '1 hours 6 min ago'],
+])('Correct time difference display', (date, expected) => {
+  const Timestamp = {
+    template: `
+      <span class="timestamp">{{ getTimeAgo(now,date) }}</span>
+  `,
+
+    data() {
+      return {
+        now: Date.now(),
+        date,
+        getTimeAgo,
+      };
+    },
+  };
+
+  const wrapper = mount(Timestamp, {
+    global: {
+      plugins: [i18n],
+    },
+  });
+
+  expect(wrapper.find('.timestamp').text()).toBe(expected);
 });
