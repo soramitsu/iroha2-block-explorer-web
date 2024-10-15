393265569af2174ba1e9af463dab0a8013126451
Vladislav Popov <papou@soramitsu.co.jp>
2024-10-15 17:39:39 +0300

    refactor(time.test): simplify test

--- src/shared/lib/time.test.ts
+++ src/shared/lib/time.test.ts
@@ -1,7 +1,9 @@
 import { expect, test } from 'vitest';
-import { formatUTC, getTimeAgo } from '@/shared/lib/time';
+import { formatUTC } from '@/shared/lib/time';
 import { mount } from '@vue/test-utils';
 import { i18n } from '@/shared/lib/localization';
+import { useTimeAgo } from '@/shared/ui/composables/useTimeAgo';
+import TimeStamp from '@/shared/ui/components/TimeStamp.vue';
 
 test.each([
   [new Date('2024-09-11T07:22:47.157Z'), 'Sep-11-2024 10:22:47 AM UTC'],
@@ -10,28 +12,19 @@ test.each([
   expect(formatUTC(input)).toBe(expected);
 });
 
-const NOW = Date.now()
+const NOW = Date.now();
 
 test.each([
   [new Date(NOW - 11000), '0 min 11 sec ago'],
   [new Date(NOW - 3599000), '59 min 59 sec ago'],
   [new Date(NOW - 4000000), '1 hours 6 min ago'],
 ])('Correct time difference display', (date, expected) => {
-  const Timestamp = {
-    template: `
-      <span class="timestamp">{{ getTimeAgo(now,date) }}</span>
-  `,
+  const { getTimeAgo } = useTimeAgo();
 
-    data() {
-      return {
-        now: Date.now(),
-        date,
-        getTimeAgo,
-      };
+  const wrapper = mount(TimeStamp, {
+    props: {
+      ...getTimeAgo(date),
     },
-  };
-
-  const wrapper = mount(Timestamp, {
     global: {
       plugins: [i18n],
     },
