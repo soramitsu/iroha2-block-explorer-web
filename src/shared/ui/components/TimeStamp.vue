611481092bcfdc8a3c5fa3620e890d71ec6ee170
Vladislav Popov <papou@soramitsu.co.jp>
2024-10-15 17:36:48 +0300

    feat(TimeStamp): add reusable component

---
+++ src/shared/ui/components/TimeStamp.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  i18nKey: string
+  firstValue: number
+  secondValue: number
+}>();
+</script>
+
+<template>
+  <span class="timestamp">{{ $t(props.i18nKey, [props.firstValue, props.secondValue]) }}</span>
+</template>
+
+<style scoped lang="scss"></style>
