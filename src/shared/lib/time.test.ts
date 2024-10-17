407ab18c8d08f9e94c0a8a6fc06033dcc2612148
Vladislav Popov <papou@soramitsu.co.jp>
2024-10-17 10:54:08 +0300

    feat(time.test): update test

--- src/shared/lib/time.test.ts
+++ src/shared/lib/time.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest';
 import { formatUTC } from '@/shared/lib/time';
 import { mount } from '@vue/test-utils';
 import { i18n } from '@/shared/lib/localization';
-import TimeAgo from '@/shared/ui/components/TimeAgo.vue';
+import TimeStamp from '@/shared/ui/components/TimeStamp.vue';
 
 test.each([
   [new Date('2024-09-11T07:22:47.157Z'), 'Sep-11-2024 10:22:47 AM UTC'],
@@ -18,8 +18,12 @@ test.each([
   [new Date(NOW - 10000), '10 secs ago'],
   [new Date(NOW - 60000), '1 min ago'],
   [new Date(NOW - 180000), '3 mins ago'],
+  [new Date(NOW - 4000000), '1 hour ago'],
+  [new Date(NOW - 18002000), '5 hours ago'],
+  [new Date(NOW - 90000000), '1 day ago'],
+  [new Date(NOW - 1800000000), '20 days ago'],
 ])('Correct time difference display', (date, expected) => {
-  const wrapper = mount(TimeAgo, {
+  const wrapper = mount(TimeStamp, {
     props: {
       value: date,
     },
