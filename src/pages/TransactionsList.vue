128724eeabe1673f02e932b641f093434bc15d23
Vladislav Popov <papou@soramitsu.co.jp>
2024-11-14 01:05:18 +0300

    fix(TransactionsTable): correct styles

--- src/pages/TransactionsList.vue
+++ src/pages/TransactionsList.vue
@@ -16,11 +16,17 @@ import BaseContentBlock from '@/shared/ui/components/BaseContentBlock.vue';
 import TransactionsTable from '@/shared/ui/components/TransactionsTable.vue';
 import { computed } from 'vue';
 import { useWindowSize } from '@vueuse/core';
-import { XL_WINDOW_SIZE } from '@/shared/ui/consts';
+import { LG_WINDOW_SIZE, XL_WINDOW_SIZE } from '@/shared/ui/consts';
 
 const { width } = useWindowSize();
 
-const hashType = computed(() => (width.value < XL_WINDOW_SIZE ? 'short' : 'full'));
+const hashType = computed(() => {
+  if (width.value >= XL_WINDOW_SIZE) return 'full';
+
+  if (width.value >= LG_WINDOW_SIZE) return 'medium';
+
+  return 'short';
+});
 </script>
 
 <style lang="scss">
