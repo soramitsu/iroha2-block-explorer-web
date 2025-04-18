4876b413df697f404ee0138deb60cc666abb9b6e
Vladislav Popov <papou@soramitsu.co.jp>
2025-04-18 11:17:41 +0300

    refactor(filter-assets): edit key

--- src/features/filter/assets/model.ts
+++ src/features/filter/assets/model.ts
@@ -3,6 +3,6 @@ import type { TabItem } from '../index';
 export type TabAssets = 'assets' | 'nft';
 
 export const ASSETS_OPTIONS: TabItem<TabAssets>[] = [
-  { i18nKey: 'assets.assets', value: 'assets' },
+  { i18nKey: 'assets.numerics', value: 'assets' },
   { i18nKey: 'assets.nfts', value: 'nft' },
 ];
